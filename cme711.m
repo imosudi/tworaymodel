0dcb435dd5312a218fcff581ae1691e853019474
imosudi <imosudi@gmail.com>
2018-02-17 02:01:32 +0100

    Initial commit

---
+++ cme711.m
@@ -0,0 +1,3 @@
+ld=0:0.001:5; d=10.^ld; ht=50; hr=2; l_gr=sqrt( (ht+hr)^2+d.^2); l_los=sqrt( (ht-hr)^2+d.^2); R=-1; G_los=1; G_gr=1; lambda=1/3; phi=2*pi*(l_gr-l_los)/lambda; los=sqrt(G_los)./l_los; gr=R*sqrt(G_gr)*exp(-j.*phi)./l_gr; rs=lambda.*(los+gr)/4*pi; ps=abs(rs).^2; norm=ps(1); pn=ps./norm; x = ld; y =10*log10(pn); plot(ld,y); title('Power vs distance plot for 2-ray ground reflected model '); xlabel('log(d)'); ylabel('Normalized Power(in dB)');
+
+print -deps graph.eps
